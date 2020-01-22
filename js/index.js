b39bb8303115d7d9842c374f568f836a8296d7cc
SkorobagatovaYaroslava <slavaskorobagatova@gmail.com>
2020-01-22 15:56:45 +0200

    created hovers for buttons, removed the scroll on the page, created anchors for the menu

--- js/index.js
+++ js/index.js
@@ -7,3 +7,19 @@ $(document).ready(function(){
 
   })
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
